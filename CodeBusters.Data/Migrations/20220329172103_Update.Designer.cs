e1a0ec83620929958bd9ce9597f306e13d984375
Troy Weaver <troylw@gmail.com>
2022-03-29 13:25:05 -0400

    Register User Tested/Working

--- CodeBusters.Data/Migrations/20220329172103_Update.Designer.cs
+++ CodeBusters.Data/Migrations/20220329172103_Update.Designer.cs
@@ -3,14 +3,16 @@ using CodeBusters.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodeBusters.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220329172103_Update")]
+    partial class Update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
