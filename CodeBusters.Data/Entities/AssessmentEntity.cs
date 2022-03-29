acf235a50e83cc0c28ba44497e4695312a5cc3ea
Hunter Browning <guccigod@Hunters-MacBook-Pro.local>
2022-03-29 14:00:24 -0400

    Added Assessment Entity

--- CodeBusters.Data/Entities/AssessmentEntity.cs
+++ CodeBusters.Data/Entities/AssessmentEntity.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,17 @@ namespace CodeBusters.Data.Entities
     public class AssessmentEntity
     {
         [Key]
-        public int Id {get; set;}
+        public int Id { get; set; }
+        [Required]
+        public string comments { get; set; }
+        [Required]
+        public int timeRequired { get; set; }
+        [Required]
+        public decimal cost { get; set; }
+        [Required]
+        public bool accepted { get; set; }
+        [ForeignKey("Id")]
+        public int ticketId { get; set; }
+        public TicketEntity TicketEntity { get; set; }
     }
 }
