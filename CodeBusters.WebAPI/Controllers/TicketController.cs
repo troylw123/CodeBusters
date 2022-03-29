1c3d7e45f75d1291d402ac5167b2f033c907ed05
mariaharrell13 <mariaharrell13@gmail.com>
2022-03-29 16:11:59 -0400

    Ticket Controller Authorization added

--- CodeBusters.WebAPI/Controllers/TicketController.cs
+++ CodeBusters.WebAPI/Controllers/TicketController.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeBusters.Services.Ticket;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeBusters.WebAPI.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class TicketController : ControllerBase
     {
-        
+        private readonly ITicketService _ticketService;
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
     }
 }
