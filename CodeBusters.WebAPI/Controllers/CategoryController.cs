150f7f628c2b708a238d637ca0f0f136ee543327
Robin Hancock <hancockrobin543@gmail.com>
2022-03-29 14:57:36 -0400

    CategoryController Added

--- CodeBusters.WebAPI/Controllers/CategoryController.cs
+++ CodeBusters.WebAPI/Controllers/CategoryController.cs
@@ -2,14 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CodeBusters.Services.Category;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CodeBusters.WebAPI.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class CategoryController : ControllerBase
     {
-        
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
     }
 }
